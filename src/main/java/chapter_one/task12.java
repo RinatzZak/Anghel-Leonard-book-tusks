928a0c30937d23ea723e9680a75921b27b7ae1fc
Dzenovich <zakirov.zrz@gmail.com>
2022-07-06 19:50:40 +0300

    task12

---
+++ src/main/java/chapter_one/task12.java
@@ -0,0 +1,33 @@
+package chapter_one;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * Написать программу удяляющую повторяющиеся символы из строки.
+ */
+
+public class task12 {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Пожалуйста, введите предложение и мы удалим из него все повторяющиеся символы");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String sentence = reader.readLine();
+            System.out.println(removeRepeatingSymbols(sentence.toLowerCase()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String removeRepeatingSymbols(String sentence) {
+        char[] chars = sentence.toCharArray();
+        Set<Character> set = new LinkedHashSet<>();
+        for (char aChar : chars) {
+            set.add(aChar);
+        }
+        return String.valueOf(set.toString());
+    }
+}
