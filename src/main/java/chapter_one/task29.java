32973278237acede0743354661b23aae7a1f77e4
Dzenovich <zakirov.zrz@gmail.com>
2022-07-09 15:11:07 +0300

    task29

---
+++ src/main/java/chapter_one/task29.java
@@ -0,0 +1,16 @@
+package chapter_one;
+
+/**
+ * Написать программу, которая сравнивает два заданных числа как беззнаковые
+ */
+
+public class task29 {
+    static int a = Integer.MAX_VALUE;
+    static int b = Integer.MIN_VALUE;
+    static int c = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        System.out.println(Integer.compareUnsigned(a, b));
+        System.out.println(Integer.compareUnsigned(a, c));
+    }
+}
