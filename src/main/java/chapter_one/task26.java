3c41a53d3923c4464b273b8c85737eb62a1eb97e
Dzenovich <zakirov.zrz@gmail.com>
2022-07-09 14:05:11 +0300

    task26

---
+++ src/main/java/chapter_one/task26.java
@@ -0,0 +1,17 @@
+package chapter_one;
+
+import java.util.function.BinaryOperator;
+
+/**
+ * Написать программу, которая которая складывает два крупных числа типа int/long и выдает переполнение операции
+ */
+
+public class task26 {
+    static int a = Integer.MAX_VALUE;
+    static int b = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        BinaryOperator<Integer> operator = Math::addExact;
+        int z = operator.apply(a, b);
+    }
+}
