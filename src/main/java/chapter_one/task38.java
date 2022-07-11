f69757cebe10438c359d21f74f56b59eaf9b755c
Dzenovich <zakirov.zrz@gmail.com>
2022-07-11 18:48:42 +0300

    task38

---
+++ src/main/java/chapter_one/task38.java
@@ -0,0 +1,16 @@
+package chapter_one;
+
+/**
+ * Написать программу, которая вычисляет сомещенное умножение-сложение
+ */
+
+public class task38 {
+    static int x = 4;
+    static int y = 5;
+    static int z = 9;
+
+    public static void main(String[] args) {
+        System.out.println(x * y + z);
+        System.out.println(Math.fma(x, y, z));
+    }
+}
