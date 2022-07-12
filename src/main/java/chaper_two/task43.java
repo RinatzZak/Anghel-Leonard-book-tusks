dce9f914e34be37bcf2fb017e54e17bfceee61b9
Dzenovich <zakirov.zrz@gmail.com>
2022-07-12 19:35:41 +0300

    refactor

--- src/main/java/chaper_two/task43.java
+++ src/main/java/chaper_two/task43.java
@@ -1,25 +1,60 @@
 package chaper_two;
 
 /**
- * Написать программу, которая
- * проверяет, находится ли заданный индекс между 0 (включительно) и заданной
- * длиной (исключительно). Если заданный индекс находится вне интервала [0;
- * заданной длины], ТО выбросить исключение IndexOutOfBoundsException
+ * Написать программу, которая выполняет проверки заданной ссылки на
+ * null, и если она не является null, то возвращает ее; в противном случае возвращает непустую ссылку по умолчанию
  */
 
 public class task43 {
-    public static int index = 30;
-    public static int length = 28;
-
     public static void main(String[] args) {
-        goodOrBadIndex(index);
+        Cat cat = new Cat("Alice", "Mow mow");
+        Cat cat1 = new Cat();
+        cat.callCat(cat);
+        cat1.callCat(cat1);
     }
 
-    public static void goodOrBadIndex(int index) {
-        if (index >= 0 && index < length) {
-            System.out.println("Yep, good index");
-        } else {
-            throw new IndexOutOfBoundsException();
+    public static class Cat{
+        String name;
+        String action;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getAction() {
+            return action;
+        }
+
+        public void setAction(String action) {
+            this.action = action;
+        }
+
+        public Cat(String name, String action) {
+            this.name = name;
+            this.action = action;
+        }
+
+        public Cat() {
+        }
+
+        @Override
+        public String toString() {
+            return "Cat{" +
+                    "name='" + name + '\'' +
+                    ", action='" + action + '\'' +
+                    '}';
+        }
+
+        public void callCat(Cat cat) {
+            if (cat != null) {
+                System.out.println(cat);
+            } else {
+                System.out.println("No cats");
+            }
         }
     }
 }
