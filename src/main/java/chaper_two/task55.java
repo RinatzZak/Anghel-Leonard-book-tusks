8f4e7787c745b578ae8ddf20bf85ca187ad7a5b0
Dzenovich <zakirov.zrz@gmail.com>
2022-07-20 11:33:48 +0300

    task55

---
+++ src/main/java/chaper_two/task55.java
@@ -0,0 +1,26 @@
+package chaper_two;
+
+/**
+ * краткий обзор инструкции и выражений switch В JDK 12
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class task55 {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String str = reader.readLine();
+            switch (str) {
+                case "astra" -> System.out.println("Yep, it's astra!");
+                case "weather" -> System.out.println("Yep, it's weather!");
+                default -> System.out.println("No, you lose this game!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
