1012076a60afd5a899f6e53d30d2008ebbb13ed8
Dzenovich <zakirov.zrz@gmail.com>
2022-07-20 13:26:07 +0300

    task56

---
+++ src/main/java/chaper_two/task56.java
@@ -0,0 +1,29 @@
+package chaper_two;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Написать фрагмент кода для примера выражения switch JDK 12 с многочисленными метками вариантов case.
+ */
+
+public class task56 {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Угадайте число от 1 до 10 включительно! \nВведите число");
+            String str = reader.readLine();
+            if (str != null) {
+                switch (str) {
+                    case "1", "2", "3", "4", "5", "6", "8", "9", "10" -> System.out.println("Нет, не это число!");
+                    case "7" -> System.out.println("Поздравляем! Вы угадали!");
+                    default -> System.out.println("Диапазон чисел от 1 до 10 включительно!");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
